b000db2fb79f096fcd56beb123c4a0ad9868c874
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-10-19 00:41:16 +0530

    :construction: Completed 12.5%

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/Header"
+
 export default function Home() {
   return (
     <main>
+      <Header></Header>
       <h1>WorkZen</h1>
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,17 @@
+import Image from 'next/image';
+
 function Header() {
   return (
-    <header>Header</header>
+    <header>
+    <Image src="/workzen.png" alt="WorkZen Logo" width={300} height={100} className="w-44 md:w-56 pb-10 md:pb-0 object-contain" />
+    <div>
+      {/*Search Bar*/}
+      <form>
+        <input type ="text" placeholder="Search" className="border-2 border-gray-300 rounded-lg p-2" />
+      </form>
+      {/*User Icon*/}
+    </div>
+    </header>
   )
 }
 
