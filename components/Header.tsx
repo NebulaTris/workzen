9153688b419f2e8d348ef46f222d6dce840c69a9
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-11-02 15:26:33 +0530

    :lipstick: Logo Changed

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,20 @@ function Header() {
   return (
     <header>
     <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10'>
+      <div className='
+      absolute 
+      top-0
+      left-0
+      w-full
+      h-96
+      bg-gradient-to-b
+      from-violet-700 to-[#0e7fc9]
+      rounded-md
+      filter
+      blur-3xl
+      opacity-50
+      -z-50'/>
+
     <Image src="/workzen.png" 
     alt="WorkZen Logo" 
     width={300} 
@@ -26,7 +40,7 @@ function Header() {
     </div>
     </div>
 
-    <div className='flex items-center justify-center px-5 md:py-5'>
+    <div className='flex items-center justify-center px-5 py-2 md:py-5'>
       <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0e7fc9] '>
         <UserCircleIcon className='inline-block h-10 w-10 text-[#0e7fc9] mr-1 '/>
         GPT is summarising your tasks for the day...
